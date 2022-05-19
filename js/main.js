03b235177cddae6d7aa469b333d3934790429e85
Nancy Chaudhry <nancych14@gmail.com>
2022-05-19 15:25:48 +0500

    Added measurements and ingredients

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@
 document.querySelector('.pick-cocktail').addEventListener('click', getDrink)
 document.querySelector('.random-cocktail').addEventListener('click', getRandom)
 
+function createListOfIngredients(measure, ingredient) {
+  const li = document.createElement('li')
+  if (measure && ingredient) {
+    li.innerText = `${measure} ${ingredient}`
+  } else {
+    li.innerText = `${ingredient}`
+  }
+  document.querySelector('ul').appendChild(li)
+}
+
+
 function getDrink() {
   const drink = document.querySelector('input').value
   const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`
@@ -11,24 +22,29 @@ function getDrink() {
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       document.querySelector('.container').classList.remove('hidden')
-      console.log(data.drinks)
       document.querySelector('h2').innerText = data.drinks[0].strDrink
       document.querySelector('img').src = data.drinks[0].strDrinkThumb
       document.querySelector('p').innerText = data.drinks[0].strInstructions
       document.querySelector('input').value = ''
       document.querySelector('ul').innerHTML = ''
 
-      const substring = 'strIngredient'
+      const ingredientSubstring = 'strIngredient'
+      const measureSubstring = 'strMeasure'
+      const drink = data.drinks[0]
+      let measure = ''
+      let ingredient = ''
 
-      for (i in data.drinks[0]) {
-        if (i.includes(substring) && data.drinks[0][i]) {
-          console.log(data.drinks[0][i])
-          const li = document.createElement('li')
-          li.innerText = data.drinks[0][i]
-          document.querySelector('ul').appendChild(li)
+      for (const [key, value] of Object.entries(drink)) {
+
+        if (key.includes(ingredientSubstring) && value) {
+          let measureKey = measureSubstring + key[key.length - 1]
+          ingredient = value
+          measure = drink[measureKey]
+          console.log(`${measure} ${ingredient}`)
+          createListOfIngredients(measure, ingredient)
         }
       }
-
+      
     })
     .catch(err => {
       console.log(`error ${err}`)
@@ -48,20 +64,23 @@ function getDrink() {
         document.querySelector('input').value = ''
         document.querySelector('ul').innerHTML = ''
 
-        const substring = 'strIngredient'
+        const ingredientSubstring = 'strIngredient'
+        const measureSubstring = 'strMeasure'
+        const drink = data.drinks[0]
+        let measure = ''
+        let ingredient = ''
 
-        
-        for (i in data.drinks[0]) {
-          if (i.includes(substring) && data.drinks[0][i]) {
-            const li = document.createElement('li')
-            li.innerText = data.drinks[0][i]
-            document.querySelector('ul').appendChild(li)
-           }
+        for (const [key, value] of Object.entries(drink)) {
+          
+          if (key.includes(ingredientSubstring) && value) {
+            let measureKey = measureSubstring + key[key.length - 1]
+            ingredient = value
+            measure = drink[measureKey]
+  
+            createListOfIngredients(measure, ingredient)
         }
-
-        console.log(data.drinks[0])
-    
-      })
+      }
+    })
       .catch(err => {
         console.log(`error ${err}`)
       })
